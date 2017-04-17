bd8521ab5e364d88ce6270ec4d93cb0a3b42ace7
murray <death.tumble@gmail.com>
2017-04-17 18:14:04 +0100

    Fixed clustering in ecs

--- cluster.tf
+++ cluster.tf
@@ -1,21 +1,26 @@
 resource "aws_ecs_service" "helloworld" {
   name            = "helloworld"
-  cluster         = "default"
-  task_definition = "helloworld:3"
+  cluster         = "helloworld"
+  task_definition = "helloworld:${aws_ecs_task_definition.helloworld.revision}"
   desired_count   = 1
+
+}
+
+resource "aws_ecs_cluster" "helloworld" {
+  name = "helloworld"
 }
 
 resource "aws_ecs_task_definition" "helloworld" {
   family = "helloworld"
   network_mode = "bridge"
-
+  
   container_definitions = <<DEFINITION
 	[
 		{
 		    "name": "helloworld",
 		    "cpu": 0,
 		    "essential": true,
-		    "image": "helloworld-http:latest",
+		    "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/helloworld-http:latest",
 		    "memory": 128,
 		    "portMappings": [
 		    	{
--- instances.tf
+++ instances.tf
@@ -1,29 +1,27 @@
-provider "aws" {
-  region     = "eu-west-1"
-}
-
-data "aws_security_group" "helloworld" {
-	name = "helloworld"
-}
-
 resource "aws_instance" "helloworld" {
 	ami = "ami-95f8d2f3"
-	availability_zone = "eu-west-1a"
+	availability_zone = "${var.availability_zone}"
 	tenancy = "default",
 	ebs_optimized = "false",
 	disable_api_termination = "false",
     instance_type= "t2.micro"
     key_name = "poc"
     monitoring = "false",
-    vpc_security_group_ids = ["${data.aws_security_group.helloworld.id}"]
-    subnet_id = "subnet-42abac34",
+    vpc_security_group_ids = ["${aws_security_group.helloworld.id}"]
+    subnet_id = "${data.aws_subnet.selected.id}",
     associate_public_ip_address = "true"
-	private_ip = "10.0.0.4"
 	source_dest_check = "true",
 	iam_instance_profile = "ecsinstancerole",
 	ipv6_address_count = "0",
-	
-	tags {
-		Environment = "poc"
-	}
+	user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=helloworld > /etc/ecs/ecs.config
+EOF
+
+  tags {
+    Name = "helloworld2"
+	Ecosystem = "${var.ecosystem}"
+	Environment = "${var.environment}"
+  }
 }
+
