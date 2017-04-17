4f2dabb6cda7c8387745327674c7b30e262ce830
murray <death.tumble@gmail.com>
2017-04-17 18:12:08 +0100

    Only bootstrap with bootstrap file

--- bootstrap.bat
+++ bootstrap.bat
@@ -3,4 +3,3 @@ terraform apply -target=aws_internet_gateway.default
 terraform apply -target=aws_internet_subnet.default
 terraform apply -target=aws_route_table.default
 terraform apply -target=aws_subnet.default
-terraform apply 
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,3 @@ terraform apply -target=aws_vpc.default
 terraform apply -target=aws_internet_gateway.default
 terraform apply -target=aws_route_table.default
 terraform apply -target=aws_subnet.default
-terraform apply 
