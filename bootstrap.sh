0170c33189a1a2164be31344cebcaf6e4ecfd5b2
Murray Tait <death.tumble@gmail.com>
2017-12-11 19:08:52 +0000

    Redoing shell scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-terraform apply -target=aws_vpc.default
-terraform apply -target=aws_internet_gateway.default
-terraform apply -target=aws_route_table.main
-
-terraform apply -target=aws_route_table.consul
-terraform apply -target=aws_subnet.consul
-terraform apply -target=aws_security_group.consul
-
-terraform apply -target=aws_route_table.weblayer
-terraform apply -target=aws_subnet.weblayer
-terraform apply -target=aws_security_group.weblayer
+source create_vpc.sh
+source create_consul.sh
+source create_weblayer.sh
 
