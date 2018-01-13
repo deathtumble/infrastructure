902b3ebb120fc0b6b2f3f69594aab40697ab1ba5
Murray Tait <death.tumble@gmail.com>
2018-01-13 17:13:38 +0000

    Using consul to route collectd

--- monitoring.tf
+++ monitoring.tf
@@ -76,6 +76,10 @@ resource "aws_ecs_task_definition" "graphite" {
   family = "graphite"
   network_mode = "host"
   volume {
+			name = "consul_config"
+			host_path = "/opt/consul/conf"
+		}
+  volume {
 			name = "grafana_data"
 			host_path = "/var/lib/grafana/"
 		}
@@ -114,6 +118,81 @@ resource "aws_ecs_task_definition" "graphite" {
   
   container_definitions = <<DEFINITION
 	[
+		{
+		    "name": "consul-agent",
+		    "cpu": 0,
+		    "essential": true,
+		    "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/consul:0.1.0",
+		    "memory": 500,
+		    "environment": [
+		    	{
+		    		"Name": "CONSUL_LOCAL_CONFIG",
+		    		"Value": "{\"leave_on_terminate\": true}"
+		    	},
+		    	{
+		    		"Name": "CONSUL_BIND_INTERFACE",
+		    		"Value": "eth0"
+		    	}, 
+		    	{
+		    		"Name": "CONSUL_CLIENT_INTERFACE",
+		    		"Value": "eth0"
+		    	}
+		    ],
+		    "command": [
+        		"agent",
+        		"-retry-join",
+        		"provider=aws tag_key=ConsulCluster tag_value=${var.nameTag}"
+      		],
+			"mountPoints": [
+                {
+                  "sourceVolume": "consul_config",
+                  "containerPath": "/consul/config",
+                  "readOnly": false
+                }
+            ],
+		    "portMappings": [
+		        {
+		          "hostPort": 8300,
+		          "containerPort": 8300,
+		          "protocol": "tcp"
+		        },
+		        {
+		          "hostPort": 8301,
+		          "containerPort": 8301,
+		          "protocol": "tcp"
+		        },
+		        {
+		          "hostPort": 8301,
+		          "containerPort": 8301,
+		          "protocol": "udp"
+		        },
+		        {
+		          "hostPort": 8302,
+		          "containerPort": 8302,
+		          "protocol": "tcp"
+		        },
+		        {
+		          "hostPort": 8302,
+		          "containerPort": 8302,
+		          "protocol": "udp"
+		        },
+		        {
+		          "hostPort": 8500,
+		          "containerPort": 8500,
+		          "protocol": "tcp"
+		        },
+		        {
+		          "hostPort": 8600,
+		          "containerPort": 8600,
+		          "protocol": "tcp"
+		        },
+		        {
+		          "hostPort": 8600,
+		          "containerPort": 8600,
+		          "protocol": "udp"
+		        }
+		    ]
+		},
 		{
 			"name": "collectd",
 			"cpu": 0,
