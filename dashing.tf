0479281b201e31104aba655ae086854a0ae30949
Murray Tait <death.tumble@gmail.com>
2018-03-17 11:26:59 +0000

    MOve the dashing service into the role module

--- dashing.tf
+++ dashing.tf
@@ -3,34 +3,10 @@ variable "dashing_subnet" {
   default = "10.0.0.144/28"
 }
 
-resource "aws_ebs_volume" "dashing" {
-  availability_zone = "${var.availability_zone}"
-  size              = 22
+module "dashing" {
+  source = "./role"
 
-  tags {
-    Name = "dashing"
-  }
-}
-
-resource "aws_volume_attachment" "dashing" {
-  device_name  = "/dev/sdh"
-  volume_id    = "${aws_ebs_volume.dashing.id}"
-  instance_id  = "${aws_instance.dashing.id}"
-  force_detach = true
-  depends_on   = ["aws_ebs_volume.dashing", "aws_instance.dashing"]
-}
-
-resource "aws_instance" "dashing" {
-  count                   = "1"
-  ami                     = "${var.ecs_ami_id}"
-  availability_zone       = "${var.availability_zone}"
-  tenancy                 = "default"
-  ebs_optimized           = "false"
-  disable_api_termination = "false"
-  instance_type           = "t2.small"
-  key_name                = "${var.key_name}"
-  private_ip              = "${var.dashing_ip}"
-  monitoring              = "false"
+  role = "dashing"
 
   vpc_security_group_ids = [
     "${aws_security_group.dashing.id}",
@@ -38,38 +14,37 @@ resource "aws_instance" "dashing" {
     "${aws_security_group.consul-client.id}",
   ]
 
-  subnet_id                   = "${aws_subnet.dashing.id}"
-  associate_public_ip_address = "true"
-  source_dest_check           = "true"
-  iam_instance_profile        = "ecsinstancerole"
-  ipv6_address_count          = "0"
+  elb_security_group   = "${aws_security_group.dashing.id}"
+  elb_instance_port    = "80"
+  elb_port             = "80"
+  healthcheck_port     = "80"
+  healthcheck_protocol = "HTTP"
+  healthcheck_path     = "/sample"
+  task_definition      = "dashing:${aws_ecs_task_definition.dashing.revision}"
+  desired_count        = "1"
 
-  user_data = <<EOF
-#!/bin/bash
-mkdir /opt/mount1
-mount /dev/xvdh /opt/mount1
-echo /dev/xvdh  /opt/mount1 ext4 defaults,nofail 0 2 >> /etc/fstab
-cat <<'EOF' >> /etc/ecs/ecs.config
-ECS_CLUSTER=dashing
-EOF
+  volume_id = ""
 
-  tags {
-    Name        = "dashing"
-    Product     = "${var.product}"
-    Environment = "${var.environment}"
+  // todo remove need to specify    
+  cidr_block = "${var.dashing_subnet}"
+
+  // globals
+  key_name                   = "${var.key_name}"
+  vpc_id                     = "${aws_vpc.default.id}"
+  gateway_id                 = "${aws_internet_gateway.default.id}"
+  availability_zone          = "${var.availability_zone}"
+  ami_id                     = "${var.ecs_ami_id}"
+  product                    = "${var.product}"
+  environment                = "${var.environment}"
+  aws_route53_record_zone_id = "${var.aws_route53_zone_id}"
+}
+
+data "template_file" "collectd-dashing" {
+  template = "${file("files/collectd.tpl")}"
+
+  vars {
+    graphite_prefix = "${var.product}.${var.environment}.dashing."
   }
-}
-
-resource "aws_ecs_cluster" "dashing" {
-  name = "dashing"
-}
-
-resource "aws_ecs_service" "dashing" {
-  name            = "dashing"
-  cluster         = "dashing"
-  task_definition = "dashing:${aws_ecs_task_definition.dashing.revision}"
-  depends_on      = ["aws_ecs_cluster.dashing", "aws_instance.dashing"]
-  desired_count   = 1
 }
 
 resource "aws_ecs_task_definition" "dashing" {
@@ -82,129 +57,32 @@ resource "aws_ecs_task_definition" "dashing" {
   }
 
   container_definitions = <<DEFINITION
-	[
+    [
+        ${data.template_file.consul_agent.rendered},
+        ${data.template_file.collectd-dashing.rendered},
         {
-            "name": "consul-agent",
+            "name": "dashing",
             "cpu": 0,
-            "essential": false,
-            "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/consul:0.1.0",
-            "memory": 500,
-            "environment": [
-                {
-                    "Name": "CONSUL_LOCAL_CONFIG",
-                    "Value": "{\"leave_on_terminate\": true}"
-                },
-                {
-                    "Name": "CONSUL_BIND_INTERFACE",
-                    "Value": "eth0"
-                }, 
-                {
-                    "Name": "CONSUL_CLIENT_INTERFACE",
-                    "Value": "lo"
-                }, 
-                {
-                    "Name": "CONSUL_ALLOW_PRIVILEGED_PORTS",
-                    "Value": ""
-                }
-            ],
-            "command": [
-                "agent",
-                "-dns-port=53",
-                "-recursor=10.0.0.2",
-                "-retry-join",
-                "provider=aws tag_key=ConsulCluster tag_value=${var.product}-${var.environment}"
-            ],
-            "mountPoints": [
-                {
-                  "sourceVolume": "consul_config",
-                  "containerPath": "/consul/config",
-                  "readOnly": false
-                }
-            ],
-            "portMappings": [
-                {
-                  "hostPort": 8300,
-                  "containerPort": 8300,
-                  "protocol": "tcp"
-                },
-                {
-                  "hostPort": 8301,
-                  "containerPort": 8301,
-                  "protocol": "tcp"
-                },
-                {
-                  "hostPort": 8301,
-                  "containerPort": 8301,
-                  "protocol": "udp"
-                },
-                {
-                  "hostPort": 8302,
-                  "containerPort": 8302,
-                  "protocol": "tcp"
-                },
-                {
-                  "hostPort": 8302,
-                  "containerPort": 8302,
-                  "protocol": "udp"
-                },
-                {
-                  "hostPort": 8500,
-                  "containerPort": 8500,
-                  "protocol": "tcp"
-                },
-                {
-                  "hostPort": 53,
-                  "containerPort": 53,
-                  "protocol": "tcp"
-                },
-                {
-                  "hostPort": 53,
-                  "containerPort": 53,
-                  "protocol": "udp"
-                }
-            ]
-        },
-		{
-			"name": "collectd",
-			"cpu": 0,
-			"dnsServers": ["127.0.0.1"],
-		    "essential": false,
-		    "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/collectd-write-graphite:0.1.1",
-		    "memory": 500,
-		    "environment": [
-		    	{
-		    		"Name": "GRAPHITE_HOST",
-		    		"Value": "graphite.service.consul"
-		    	}, 
-		    	{
-		    		"Name": "GRAPHITE_PREFIX",
-		    		"Value": "${var.product}.${var.environment}.dashing."
-		    	}
-		    ]
-		},
-		{
-			"name": "dashing",
-			"cpu": 0,
-		    "essential": true,
-		    "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/smashing:latest",
-		    "memory": 200,
+            "essential": true,
+            "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/smashing:latest",
+            "memory": 200,
             "environment": [
                 {
                     "Name": "PORT",
                     "Value": "80"
                 }
              ], 
-		    "portMappings": [
-		        {
-		          "hostPort": 80,
-		          "containerPort": 80,
-		          "protocol": "tcp"
-		        }
-		    ]
-		},
-		{
-		    "name": "aws-proxy",
-		    "cpu": 0,
+            "portMappings": [
+                {
+                  "hostPort": 80,
+                  "containerPort": 80,
+                  "protocol": "tcp"
+                }
+            ]
+        },
+        {
+            "name": "aws-proxy",
+            "cpu": 0,
             "essential": true,
             "image": "453254632971.dkr.ecr.eu-west-1.amazonaws.com/aws_proxy:0.1.0-SNAPSHOT",
             "memory": 200,
@@ -229,87 +107,9 @@ resource "aws_ecs_task_definition" "dashing" {
                   "protocol": "tcp"
                 }
             ]
-		}
-	]
+        }
+    ]
     DEFINITION
-}
-
-resource "aws_lb_cookie_stickiness_policy" "dashing" {
-  name                     = "dashing"
-  load_balancer            = "${aws_elb.dashing.id}"
-  lb_port                  = 80
-  cookie_expiration_period = 600
-}
-
-resource "aws_elb" "dashing" {
-  name            = "dashing"
-  security_groups = ["${aws_security_group.dashing.id}"]
-  subnets         = ["${aws_subnet.dashing.id}"]
-  depends_on      = ["aws_security_group.dashing"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/sample"
-    interval            = 30
-  }
-}
-
-resource "aws_route53_record" "dashing" {
-  zone_id = "${var.aws_route53_zone_id}"
-  name    = "dashing"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${aws_elb.dashing.dns_name}"]
-}
-
-resource "aws_elb_attachment" "dashing" {
-  count    = "1"
-  elb      = "${aws_elb.dashing.id}"
-  instance = "${aws_instance.dashing.id}"
-}
-
-resource "aws_route_table" "dashing" {
-  vpc_id = "${aws_vpc.default.id}"
-
-  tags {
-    Name        = "dashing-${var.product}-${var.environment}"
-    Product     = "${var.product}"
-    Environment = "${var.environment}"
-    Layer       = "dashing"
-  }
-}
-
-resource "aws_route" "dashing" {
-  route_table_id         = "${aws_route_table.dashing.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-
-  depends_on = ["aws_route_table.dashing", "aws_internet_gateway.default"]
-}
-
-resource "aws_subnet" "dashing" {
-  vpc_id            = "${aws_vpc.default.id}"
-  cidr_block        = "${var.dashing_subnet}"
-  availability_zone = "${var.availability_zone}"
-
-  tags {
-    Name = "dashing-${var.product}-${var.environment}"
-  }
-}
-
-resource "aws_route_table_association" "dashing" {
-  subnet_id      = "${aws_subnet.dashing.id}"
-  route_table_id = "${aws_route_table.dashing.id}"
-  depends_on     = ["aws_route_table.dashing", "aws_subnet.dashing"]
 }
 
 resource "aws_security_group" "dashing" {
@@ -326,10 +126,10 @@ resource "aws_security_group" "dashing" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8082
+    to_port     = 8082
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.admin_cidr}", "${var.vpc_cidr}"]
   }
 
   egress {
