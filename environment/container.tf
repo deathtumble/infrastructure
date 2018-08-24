117721c29c6349f2be4415511274748c13b1b87c
Murray Tait <death.tumble@gmail.com>
2018-08-24 22:38:38 +0100

    Removing unused template files

--- environment/container.tf
+++
@@ -1,8 +0,0 @@
-data "template_file" "consul_agent" {
-  template = "${file("${path.module}/files/consul-agent.tpl")}"
-
-  vars {
-    nameTag           = "${local.product}-${local.environment}"
-    consul_docker_tag = "${var.consul_docker_tag}"
-  }
-}
