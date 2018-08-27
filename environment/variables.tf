2b8c80b3322860ab1893c14e9cc61592f4ff049b
Murray Tait <death.tumble@gmail.com>
2018-08-27 09:12:22 +0100

    Combining defaults and variables files

--- environment/variables.tf
+++ environment/variables.tf
@@ -1,3 +1,53 @@
+variable "region" {
+  type    = "string"
+  default = "eu-west-1"
+}
+
+variable "availability_zone_1" {
+  type    = "string"
+  default = "eu-west-1c"
+}
+
+variable "availability_zone_2" {
+  type    = "string"
+  default = "eu-west-1b"
+}
+
+variable "concourse_task_status" {
+  type    = "string"
+  default = "up"
+}
+
+variable "nexus_task_status" {
+  type    = "string"
+  default = "up"
+}
+
+variable "dashing_task_status" {
+  type    = "string"
+  default = "up"
+}
+
+variable "aws_proxy_task_status" {
+  type    = "string"
+  default = "up"
+}
+
+variable "grafana_task_status" {
+  type    = "string"
+  default = "up"
+}
+
+variable "prometheus_task_status" {
+  type    = "string"
+  default = "up"
+}
+
+variable "consul_task_status" {
+  type    = "string"
+  default = "up"
+}
+
 variable "globals" {
   type = "map"
 
