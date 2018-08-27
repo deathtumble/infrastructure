33018b139aacad020ad29fce8043d8d8fa74881e
Murray Tait <death.tumble@gmail.com>
2018-08-27 09:03:13 +0100

    Extract aws-proxy module

--- environment/default-cluster.tf
+++ environment/default-cluster.tf
@@ -11,7 +11,7 @@ module "default-instance" {
   cluster_name      = "default"
 
   vpc_security_group_ids = [
-    "${aws_security_group.aws-proxy.id}",
+    "${module.aws-proxy.aws_security_group_id}",
     "${module.dashing.aws_security_group_id}",
     "${module.concourse_web.aws_security_group_id}",
     "${aws_security_group.os.id}",
