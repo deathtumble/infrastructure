0c14277219421538b97a0f00280e5776d7aefe19
murray <murray@proteus>
2019-04-25 18:25:36 +0100

    removing outputs

--- environment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_alb_default_dns_name" {
-  value = module.vpc.aws_alb_default_dns_name
-}
-
--- murray/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_alb_default_dns_name" {
-  value = module.environment.aws_alb_default_dns_name
-}
-
